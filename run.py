19984327913326dfcc5d1864a5285ddfee7b98ef
NJMIXI98 <63385379+NJMIXI98@users.noreply.github.com>
2022-12-05 22:08:46 -0800

    Update run.py

--- run.py
+++ run.py
@@ -11,6 +11,11 @@ def main(targets):
     df = get_data(targets)
     average_price(df)
     model(df)
+    plot(df)
+    gender_ranks = create_ranks(df,'gender')
+    race_ranks = create_ranks(df,'ethnicity')
+    rank_plot(gender_ranks)
+    rank_plot(race_ranks)
     
 
 if __name__ == '__main__':
