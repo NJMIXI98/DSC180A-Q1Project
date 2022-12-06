9898af9a69c432c66b94ec3c2cee49a166574b04
NJMIXI98 <63385379+NJMIXI98@users.noreply.github.com>
2022-12-05 23:25:38 -0800

    Update run.py

--- run.py
+++ run.py
@@ -18,23 +18,20 @@ def main(targets):
         print("I put raw data in google drive!")
         
     if 'test' in targets:
-        #with open('test/test.json') as fh:
-            #df = get_data(fh)  
-        print('1111111')
         df = get_data('test/test.json')
         a= average_price(df)
         print(a)
         b= model(df)
         print(b)
         c= plot(df)
-        plot.show()
+        plt.show()
         gender_ranks = create_ranks(df,'gender')
         race_ranks = create_ranks(df,'ethnicity')
         d= rank_plot(gender_ranks)
-        plot.show()
+        plt.show()
         #print(d)
         e= rank_plot(race_ranks)
-        plot.show()
+        plt.show()
         #print(e)
       
 
