c75ad3937b6f43b7bfcedc971741e6f9c1b32659
NJMIXI98 <63385379+NJMIXI98@users.noreply.github.com>
2022-12-05 22:11:13 -0800

    Add files via upload

--- src/q1.py
+++ src/q1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 import numpy as np
@@ -13,20 +13,7 @@ from statsmodels.graphics.factorplots import interaction_plot
 import matplotlib.pyplot as plt
 
 
-# In[137]:
-
-
-df = pd.read_json('test.json', lines=True)
-
-
-# In[139]:
-
-
-df['gender'] = df.apply(lambda row: row.treatment.get('gender'), axis=1)
-df['ethnicity'] = df.apply(lambda row: row.treatment.get('ethnicity'), axis=1)
-
-
-# In[140]:
+# In[2]:
 
 
 def price_row(row):
@@ -37,20 +24,7 @@ def price_row(row):
         return '0'
 
 
-# In[141]:
-
-
-df['position'] = df.apply(lambda row: price_row(row), axis=1)
-
-
-# In[142]:
-
-
-df = df[['scraper','gender','ethnicity','position']]
-df = df.dropna()
-
-
-# In[143]:
+# In[3]:
 
 
 def price(row):
@@ -67,13 +41,7 @@ def price(row):
     return ls
 
 
-# In[144]:
-
-
-df['price'] = df.apply(lambda row: price(row) , axis=1)
-
-
-# In[145]:
+# In[4]:
 
 
 def locate(row):
@@ -89,137 +57,67 @@ def locate(row):
                     return None
 
 
-# In[146]:
+# In[5]:
 
 
-df['locality'] = df.apply(lambda row:locate(row),axis=1)
+def get_data(outfir):
+    df = pd.read_json(outfir, lines=True)
+    df['gender'] = df.apply(lambda row: row.treatment.get('gender'), axis=1)
+    df['ethnicity'] = df.apply(lambda row: row.treatment.get('ethnicity'), axis=1)
+    df['position'] = df.apply(lambda row: price_row(row), axis=1)
+    df = df[['scraper','gender','ethnicity','position']]
+    df = df.dropna()
+    df['price'] = df.apply(lambda row: price(row) , axis=1)
+    df['locality'] = df.apply(lambda row:locate(row),axis=1)
+    df = df.dropna()
+    df = df.reset_index(drop=True)
+    df['price_mean'] = df.apply(lambda row: round(mean(row['price'])) , axis=1)
+    return df
 
 
-# In[147]:
+# In[6]:
 
 
-df = df.dropna()
-df = df.reset_index(drop=True)
+def average_price(df):
+    get = df.groupby(['scraper','locality','gender', 'ethnicity']).agg({'price_mean': ['mean']}).astype(int)
+    return get
 
 
-# In[148]:
+# In[7]:
 
 
-df['price_mean'] = df.apply(lambda row: round(mean(row['price'])) , axis=1)
+def model(df):
+    out = ols('price_mean ~ C(gender) + C(ethnicity) + C(gender):C(ethnicity)', data=df).fit()
+    return sm.stats.anova_lm(out, typ=2)
 
 
-# In[ ]:
+# In[14]:
 
 
-
-
-
-# In[149]:
-
-
-average_price_groups = df.groupby(['scraper','locality','gender', 'ethnicity']).agg({'price_mean': ['mean']}).astype(int)
-
-
-# In[150]:
-
-
-average_price_groups
-
-
-# In[158]:
-
-
-model_R = ols('price_mean ~ C(gender) + C(ethnicity) + C(gender):C(ethnicity)', data=df).fit()
-
-
-# In[159]:
-
-
-sm.stats.anova_lm(model, typ=2)
-
-
-# In[151]:
-
-
-Realtor = df[df.scraper == 'Realtor']
-
-
-# In[152]:
-
-
-model_R = ols('price_mean ~ C(gender) + C(ethnicity) + C(gender):C(ethnicity)', data=Realtor).fit()
-
-
-# In[153]:
-
-
-sm.stats.anova_lm(model_R, typ=2)
-
-
-# In[154]:
-
-
-Trulia = df[df.scraper == 'Trulia']
-
-
-# In[155]:
-
-
-model_T = ols('price_mean ~ C(gender) + C(ethnicity) + C(gender):C(ethnicity)', data=Trulia).fit()
-
-
-# In[156]:
-
-
-sm.stats.anova_lm(model_T, typ=2)
-
-
-# In[160]:
-
-
-fig = interaction_plot(x=df['ethnicity'], trace=df['gender'], response=df['price_mean'], 
+def plot(df):
+    fig = interaction_plot(x=df['ethnicity'], trace=df['gender'], response=df['price_mean'], 
     colors=['#d17a22', '#4c061d'])
-plt.show()
+    return plt.show()
 
 
-# In[164]:
+# In[26]:
 
 
-df[['rank1','rank2','rank3','rank4','rank5','rank6','rank7','rank8','rank9','rank10']] = pd.DataFrame(df.price.tolist(), index= df.index)
-
-
-# In[173]:
-
-
-gender_ranks = df.groupby(['gender']).agg({'rank1': ['mean'],'rank2': ['mean'],'rank3': ['mean'],'rank4': ['mean'],
+def create_ranks(df,rk):
+    df[['rank1','rank2','rank3','rank4','rank5','rank6','rank7','rank8','rank9','rank10']] = pd.DataFrame(df.price.tolist(), index= df.index)
+    ranks = df.groupby([rk]).agg({'rank1': ['mean'],'rank2': ['mean'],'rank3': ['mean'],'rank4': ['mean'],
                                            'rank5': ['mean'],'rank6': ['mean'],'rank7': ['mean'],'rank8': ['mean'],
                                            'rank9': ['mean'],'rank10': ['mean']}).astype(int)
-gender_ranks
+    return ranks
 
 
-# In[189]:
+# In[24]:
 
 
-gender_ranks.T.plot()
-plt.xlabel('Ranking')
-plt.ylabel('Average Price')
-
-
-# In[174]:
-
-
-ethnicity_ranks = df.groupby(['ethnicity']).agg({'rank1': ['mean'],'rank2': ['mean'],'rank3': ['mean'],'rank4': ['mean'],
-                                           'rank5': ['mean'],'rank6': ['mean'],'rank7': ['mean'],'rank8': ['mean'],
-                                           'rank9': ['mean'],'rank10': ['mean']}).astype(int)
-ethnicity_ranks
-
-
-# In[190]:
-
-
-ethnicity_ranks.T.plot()
-plt.xlabel('Ranking')
-plt.ylabel('Average Price')
+def rank_plot(ranks):
+    ranks.T.plot()
+    plt.xlabel('Ranking')
+    plt.ylabel('Average Price')
 
 
 # In[ ]:
